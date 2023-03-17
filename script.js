b1eef9d0c5ab1ae9ef9eae814f7bac7c0f5dd133
Kash <64434204+ykl-commits@users.noreply.github.com>
2023-03-17 17:04:03 -0500

    Complete Etch-a-Sketch

---
+++ script.js
@@ -0,0 +1,90 @@
+const gridContainer = document.querySelector('#grid-container');
+let isRainbowGrid = false;
+
+
+function createGrid(rows, columns, isRainbow) {
+    gridContainer.innerHTML = '';
+    let isMouseDown = false;
+    if (isRainbow) {
+        isRainbowGrid = true;
+    } else {
+        isRainbowGrid = true;
+    }
+    for (let i = 0; i < rows; i++){
+        for (let j = 0; j < columns; j++) {
+            const gridSquare = document.createElement('div')
+            gridSquare.classList.add('grid-square');
+            gridSquare.style.border = '1px solid rgba(0, 0, 0, 0.5)';
+            gridContainer.appendChild(gridSquare);
+
+            gridSquare.addEventListener('mouseover', () => {
+                if (isMouseDown) {
+                    gridSquare.style.backgroundColor = 'white';
+                } else {
+                    if (isRainbow){
+                        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                        gridSquare.style.backgroundColor = '#' + randomColor;
+                    } else {
+                    gridSquare.style.backgroundColor = 'black'
+                    }
+                }
+            });
+
+            gridSquare.addEventListener('mousedown', () => {
+                isMouseDown = true;
+                gridSquare.style.backgroundColor = 'white';
+            });
+
+            gridSquare.addEventListener('mouseup', () => {
+                isMouseDown = false;
+            });
+        }
+    }
+
+    gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+
+
+}
+
+const rainbowButton = document.querySelector('#rainbow');
+
+rainbowButton.addEventListener('click', () => {
+    const rows = parseInt(document.querySelector('#rows').value);
+    const columns = parseInt(document.querySelector('#columns').value);
+
+    createGrid(rows, columns, true);
+});
+
+const submitButton = document.querySelector(`#submit-button`);
+
+submitButton.addEventListener('click', () => {
+    const rows = parseInt(document.querySelector('#rows').value);
+    const columns = parseInt(document.querySelector('#columns').value);
+
+    if (rows >= 1 && rows <= 100 && columns >= 1 && columns <= 100) {
+        createGrid(rows, columns, isRainbowGrid);
+    } else {
+        alert('Pleae enter values between 1 and 100.')
+    }
+});
+
+
+const resetButton = document.querySelector('#reset-button');
+
+resetButton.addEventListener('click', () => {
+
+    const rows = parseInt(document.querySelector('#rows').value);
+    const columns = parseInt(document.querySelector('#columns').value);
+    createGrid(rows, columns);
+})
+
+function resetGrid () {
+    const rows = parseInt(document.querySelector('#rows').value);
+    const columns = parseInt(document.querySelector('#columns').value);
+    createGrid(rows, columns, isRainbowGrid);
+}
+
+resetButton.addEventListener('click', resetGrid);
+
+
